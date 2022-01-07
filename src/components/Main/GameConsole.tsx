99b0ea82ac086ba38275e6f391318eb93f091654
jefersonrgomes <jeferson.rgomes@hotmail.com>
2022-01-07 09:36:07 -0300

    update styles

--- src/components/Main/GameConsole.tsx
+++ src/components/Main/GameConsole.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import { Console } from './styles'
+import React from 'react';
+import { Console, Gui } from './styles';
 
 export default function GameConsole() {
   return (
-      <Console>
+    <div className="game__otions">
+      <h1 className="game__title">Genius</h1>
+      <Gui>
+        <div className="group">
+          <div className="gui__counter">--</div>
+          <p className="gui__label">Count</p>
+        </div>
 
-      
-      </Console>
+        <div className="group">
+          <div className="gui__led"></div>
+          <div className="gui__btn gui__btn--start"></div>
+          <p className="gui__label">Start</p>
+        </div>
+
+        <div className="group">
+          <div className="gui__led"></div>
+          <div className="gui__btn gui__btn--strict"></div>
+          <p className="gui__label">Strict</p>
+        </div>
+
+        <div className="group group-large">
+          <p className="gui__label gui__label--switch">ON</p>
+          <div className="gui__btn-switch"></div>
+          <p className="gui__label gui__label--switch">OFF</p>
+        </div>
+      </Gui>
+    </div>
   );
 }
