b5488a4672183a68f5dd2d93b8cb5b04cdc2f2c8
jefersonrgomes <jeferson.rgomes@hotmail.com>
2022-01-29 13:32:44 -0300

    update display score

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+/*** *** *** *** ***/
+/*    ATRIBUTES    */
+/*** *** *** *** ***/
+
 const _data = {
 	gameOn: false,
 	timeout: undefined,
@@ -39,19 +43,25 @@ _gui.switch.addEventListener("click", () => {
 	_data.score = 0;
 	_data.gameSequence = [];
 	_data.playerSequence = [];
-	disablePads();
+		disablePads();
 	_gui.led.classList.remove("gui__led--active");
-});
+}); 
 
 _gui.strict.addEventListener("click", () => {
 	if(!_data.gameOn) return;	
 	_data.strict = _gui.led.classList.toggle("gui__led--active");
 });
 
+
+/*** FUN DIABLE PADS ***/
+
+
 _gui.start.addEventListener("click", () => {
 	startGame();
+	console.log('clicado start')
 });
 
+/*** FUN PAD LISTENER ***/
 const padListener = (e) => {
 
 }
@@ -60,72 +70,72 @@ _gui.pads.forEach(pad => {
 	pad.addEventListener("click", padListener);
 });
 
+/*** FUN START GAME ***/
 const startGame = () => {
 	blink("--", () => {
+		newColor();
 		console.log("Game iniciado com sucesso!");
 	})
 }
 
+/*** FUN SET SCORE ***/
 const setScore = () => {
-
+	const score = _data.score.toString();
+	const display = "00".substring(0, 2 - score.length) + score;
+	_gui.counter.innerHTML = display;
 }
 
+/*** FUN NEW COLOR ***/
 const newColor = () => {
-
+	_data.gameSequence.push =  Math.floor(Math.random() * 4);
+	_data.score++;
+	setScore();
 }
 
+/*** FUN PLAY SEQUENCE ***/
 const playSequence = () => {
 
 }
 
+
+/*** FUN BLINK DISPLAY ***/
 const blink = (text, callback) => {
 	let counter = 0,
 		on = true;
-	
 	_gui.counter.innerHTML = text;
 
 	const interval = setInterval(() => {
-		if (on)
-		{
+		if (on) {
 			_gui.counter.classList.remove("gui__counter--on");
 		}
 		else {
 			_gui.counter.classList.add("gui__counter--on");
-			
+
 			if (++counter === 3) {
 				clearInterval(interval)
 				callback();
 			}
 		}
-
 		on = !on;
 	}, 150);
-	
 }
 
+/*** FUN WAIT FOR PLAYER CLIC ***/
 const waitForPlayerClick = () => {
-
 }
 
+/*** FUN RESET OR PLAY AGAIN ***/
 const resetOrPlayAgain = () => {
 
 }
 
+/*** FUN CHANCGE PAD CURSOR ***/
 const changePadCursor = (cursorType) => {
 
 }
-
+/*** FUN DISABLE PADS ***/
 const disablePads = () => {
 	_gui.pads.forEach(pad => {
 		pad.classList.remove("game__pad--active");
 	})
-	
-
-}
-
-const switchOn = () => {
-	if (_data.gameOn)
-	{
-		
-	 }
 }
